0759465bdf9b7dc1bc61883735b3fb61c488d3b5
Douglas Crockford <douglascrockford@github.com>
2013-02-19 04:41:16 -0800

    RuntimeException

--- JSONException.java
+++ JSONException.java
@@ -2,26 +2,36 @@ package org.json;
 
 /**
  * The JSONException is thrown by the JSON.org classes when things are amiss.
+ *
  * @author JSON.org
- * @version 2010-12-24
+ * @version 2013-02-10
  */
-public class JSONException extends Exception {
+public class JSONException extends RuntimeException {
     private static final long serialVersionUID = 0;
     private Throwable cause;
 
     /**
      * Constructs a JSONException with an explanatory message.
-     * @param message Detail about the reason for the exception.
+     *
+     * @param message
+     *            Detail about the reason for the exception.
      */
     public JSONException(String message) {
         super(message);
     }
 
+    /**
+     *  Constructs a new JSONException with the specified cause.
+     */
     public JSONException(Throwable cause) {
         super(cause.getMessage());
         this.cause = cause;
     }
 
+    /**
+     * Returns the cause of this throwable or null if the cause is nonexistent or unknown.
+     * @returns the cause of this throwable or null if the cause is nonexistent or unknown.
+     */
     public Throwable getCause() {
         return this.cause;
     }
