a01a8dfc4376bbaa67c0036563dfd2ed2dcd432b
Martin Blom <martin@blom.org>
2014-02-07 23:45:04 +0100

    JSONObject.numberToString() now outputs large Double integers as integers.
    For instance, 99999999 is now serialized as "99999999" instead of "9.9999999E7"

--- JSONObject.java
+++ JSONObject.java
@@ -771,18 +771,12 @@ public class JSONObject {
         testValidity(number);
 
 // Shave off trailing zeros and decimal point, if possible.
-
-        String string = number.toString();
-        if (string.indexOf('.') > 0 && string.indexOf('e') < 0
-                && string.indexOf('E') < 0) {
-            while (string.endsWith("0")) {
-                string = string.substring(0, string.length() - 1);
-            }
-            if (string.endsWith(".")) {
-                string = string.substring(0, string.length() - 1);
-            }
+        if (number.doubleValue() == number.longValue()) {
+            return "" + number.longValue();
         }
-        return string;
+        else {
+            return number.toString();
+        }
     }
 
     /**
