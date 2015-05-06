a851bf095196aab53b7406a55f2f7c0125413a60
stleary <stleary@gmail.com>
2015-05-05 20:11:28 -0500

    Replaced tab chars, updated versions

--- Property.java
+++ Property.java
@@ -31,7 +31,7 @@ import java.util.Properties;
 /**
  * Converts a Property file data into JSONObject and back.
  * @author JSON.org
- * @version 2014-05-03
+ * @version 2015-05-05
  */
 public class Property {
     /**
@@ -43,7 +43,7 @@ public class Property {
     public static JSONObject toJSONObject(java.util.Properties properties) throws JSONException {
         JSONObject jo = new JSONObject();
         if (properties != null && !properties.isEmpty()) {
-            Enumeration enumProperties = properties.propertyNames();
+            Enumeration<?> enumProperties = properties.propertyNames();
             while(enumProperties.hasMoreElements()) {
                 String name = (String)enumProperties.nextElement();
                 jo.put(name, properties.getProperty(name));
