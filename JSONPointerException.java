bff352791c4753dfe871a1bf6093f60fb6213447
Bence Erős <ebence88@gmail.com>
2016-04-27 00:10:45 +0200

    removed @author tag from JSONPointerException

--- JSONPointerException.java
+++ JSONPointerException.java
@@ -3,9 +3,6 @@ package org.json;
 /**
  * The JSONPointerException is thrown by {@link JSONPointer} if an error occurs
  * during evaluating a pointer.
- * 
- * @author erosb
- *
  */
 public class JSONPointerException extends JSONException {
     private static final long serialVersionUID = 8872944667561856751L;
