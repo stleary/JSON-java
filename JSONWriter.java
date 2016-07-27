bec3919688337fa549ef7e40a24ff3b3f92686ea
run2000 <run2000@gmail.com>
2016-07-28 09:32:09 +1000

    Update version string

--- JSONWriter.java
+++ JSONWriter.java
@@ -53,7 +53,7 @@ SOFTWARE.
  * <p>
  * This can sometimes be easier than using a JSONObject to build a string.
  * @author JSON.org
- * @version 2015-12-09
+ * @version 2016-07-28
  */
 public class JSONWriter {
     private static final int maxdepth = 200;
