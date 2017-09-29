c695ee28276e291ab3e499002832f851a1708737
Valeriy <vp_inbox@ukr.net>
2017-09-30 01:30:31 +0300

    Refactoring related with adding new interface.(fix misprint)

--- JSONable.java
+++ JSONable.java
@@ -2,6 +2,6 @@ package org.json;
 
 public interface JSONable {
 	default public JSONObject toJSONObject() {
-		return new JSONObject(JSONObject.createJSONObject(this));
+		return JSONObject.createJSONObject(this);
 	}
 }
