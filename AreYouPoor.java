12650d5a933741832028860029719b9d53a65358
Mohammad Izamul Fikri Fahmi <91443233+zamfyy@users.noreply.github.com>
2021-10-10 09:58:19 +0700

    Create AreYouPoor.java

---
+++ AreYouPoor.java
@@ -0,0 +1,17 @@
+package JSON-java;
+import java.util.Scanner;
+
+public class AreYouPoor {
+  
+	public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  int dollar;
+  System.out.printn("Input your salary per day in dollar: ";dollar = sc.nextInt;
+  
+  if(dollar <50){
+  System.out.println("you are poor people, lets work more hard");
+  }else{
+  System.out.println("You are rich people, don't forget to sharing and be kind to other";
+  }
+  }
+}
