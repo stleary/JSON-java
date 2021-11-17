4565bddcbb227ccebc36e145d01966859f0db69b
Zach <zehaom@umich.edu>
2021-11-17 17:23:40 -0600

    data class for recursive test

---
+++ src/test/java/org/json/junit/data/RecursiveBean.java
@@ -0,0 +1,22 @@
+package org.json.junit.data;
+
+/**
+ * test class for verifying if recursively defined bean can be correctly identified
+ * @author Zetmas
+ *
+ */
+public class RecursiveBean {
+    private String name;
+    private Object reference;
+    public String getName() { return name; }
+    public Object getRef() {return reference;}
+
+    public RecursiveBean(String name) {
+        this.name = name;
+        reference = null;
+    }
+    public RecursiveBean(String name, Object refObj) {
+        this.name = name;
+        reference = refObj;
+    }
+}
