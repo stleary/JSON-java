a2c0562e04badc81259cf1d703a52aba7965b464
Dean <dean.kane@opencastsoftware.com>
2022-10-07 15:04:09 +0100

    Removed unused import

--- src/test/java/org/json/junit/XMLTest.java
+++ src/test/java/org/json/junit/XMLTest.java
@@ -18,7 +18,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
