385500ab1af411caffc58f6c279525cdfda87eeb
33447142+ZachsCoffee <33447142+ZachsCoffee@users.noreply.github.com>
2023-10-17 00:05:18 +0300

    Enhance the docs

--- src/main/java/org/json/JSONParserConfiguration.java
+++ src/main/java/org/json/JSONParserConfiguration.java
@@ -1,9 +1,15 @@
 package org.json;
 
+/**
+ * Configuration class for JSON parsers.
+ */
 public class JSONParserConfiguration extends AbstractConfiguration {
 
     private boolean circularDependencyValidated;
 
+    /**
+     * Configuration with the default values.
+     */
     public JSONParserConfiguration() {
         circularDependencyValidated = true;
     }
@@ -12,10 +18,22 @@ public class JSONParserConfiguration extends AbstractConfiguration {
         this.circularDependencyValidated = circularDependencyValidated;
     }
 
+    /**
+     * Retrieves the configuration about the circular dependency check.
+     *
+     * @return if true enables the circular dependencies check, false otherwise
+     */
     public boolean isCircularDependencyValidated() {
         return circularDependencyValidated;
     }
 
+    /**
+     * Sets the flag that controls the underline functionality to check or not about circular dependencies.
+     *
+     * @param circularDependencyValidation if true enables the circular dependencies check, false otherwise.
+     *                                     Default is true
+     * @return a new instance of the configuration with the given value being set
+     */
     public JSONParserConfiguration withCircularDependencyValidation(boolean circularDependencyValidation) {
         return with(newInstance -> newInstance.circularDependencyValidated = circularDependencyValidation);
     }
