9df8a09416a410401a1def296fbcbcbe61e06cc7
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-10-16 17:58:10 -0700

    fix(JSONPointerException): add constructor params

--- src/main/java/org/json/JSONPointerException.java
+++ src/main/java/org/json/JSONPointerException.java
@@ -14,10 +14,21 @@ Public Domain.
 public class JSONPointerException extends JSONException {
     private static final long serialVersionUID = 8872944667561856751L;
 
+    /**
+     * Constructs a new JSONPointerException with the specified detail message.
+     *
+     * @param message the detail message.
+     */
     public JSONPointerException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new JSONPointerException with the specified detail message and cause.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method).
+     * @param cause the cause (which is saved for later retrieval by the getCause() method).
+     */
     public JSONPointerException(String message, Throwable cause) {
         super(message, cause);
     }
