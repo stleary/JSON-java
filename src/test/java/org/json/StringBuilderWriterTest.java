d672b44a259868dc85c6bd090cb80f1c1051ae15
Simulant <nfaupel.dev@gmail.com>
2024-02-24 21:29:28 +0100

    #863 add StringBuilderWriter unit test

---
+++ src/test/java/org/json/StringBuilderWriterTest.java
@@ -0,0 +1,59 @@
+package org.json;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringBuilderWriterTest {
+    private StringBuilderWriter writer;
+
+    @Before
+    public void setUp() {
+        writer = new StringBuilderWriter();
+    }
+
+    @Test
+    public void testWriteChar() {
+        writer.write('a');
+        assertEquals("a", writer.toString());
+    }
+
+    @Test
+    public void testWriteCharArray() {
+        char[] chars = {'a', 'b', 'c'};
+        writer.write(chars, 0, 3);
+        assertEquals("abc", writer.toString());
+    }
+
+    @Test
+    public void testWriteString() {
+        writer.write("hello");
+        assertEquals("hello", writer.toString());
+    }
+
+    @Test
+    public void testWriteStringWithOffsetAndLength() {
+        writer.write("hello world", 6, 5);
+        assertEquals("world", writer.toString());
+    }
+
+    @Test
+    public void testAppendCharSequence() {
+        writer.append("hello");
+        assertEquals("hello", writer.toString());
+    }
+
+    @Test
+    public void testAppendCharSequenceWithStartAndEnd() {
+        CharSequence csq = "hello world";
+        writer.append(csq, 6, 11);
+        assertEquals("world", writer.toString());
+    }
+
+    @Test
+    public void testAppendChar() {
+        writer.append('a');
+        assertEquals("a", writer.toString());
+    }
+}
