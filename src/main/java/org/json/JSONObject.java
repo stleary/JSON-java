df0e3e9ab73d99f1256055a17bd86c8a1a000b59
Simulant <nfaupel.dev@gmail.com>
2024-02-25 09:47:40 +0100

    add a comment explaining the ordering

--- src/main/java/org/json/JSONObject.java
+++ src/main/java/org/json/JSONObject.java
@@ -2701,6 +2701,7 @@ public class JSONObject {
         if (value == null || value.equals(null)) {
             writer.write("null");
         } else if (value instanceof JSONString) {
+            // JSONString must be checked first, so it can overwrite behaviour of other types below
             Object o;
             try {
                 o = ((JSONString) value).toJSONString();
