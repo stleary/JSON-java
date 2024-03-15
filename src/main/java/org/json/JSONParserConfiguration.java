dcbbccc76cd2a822ac0069f4f65b18accd8a9306
rikkarth <ricardominamendes@outlook.com>
2024-03-15 00:19:25 +0000

    feat(#871-strictMode): strictMode configuration add to JSONParserConfiguration
    docs(#871-strictMode): add javadoc

--- src/main/java/org/json/JSONParserConfiguration.java
+++ src/main/java/org/json/JSONParserConfiguration.java
@@ -4,10 +4,18 @@ package org.json;
  * Configuration object for the JSON parser. The configuration is immutable.
  */
 public class JSONParserConfiguration extends ParserConfiguration {
+
     /**
      * Used to indicate whether to overwrite duplicate key or not.
      */
     private boolean overwriteDuplicateKey;
+
+    /**
+     * This flag, when set to true, instructs the parser to throw a JSONException if it encounters an invalid character
+     * immediately following the final ']' character in the input. This is useful for ensuring strict adherence to the
+     * JSON syntax, as any characters after the final closing bracket of a JSON array are considered invalid.
+     */
+    private boolean strictMode;
 
     /**
      * Configuration with the default values.
@@ -26,10 +34,9 @@ public class JSONParserConfiguration extends ParserConfiguration {
     }
 
     /**
-     * Defines the maximum nesting depth that the parser will descend before throwing an exception
-     * when parsing a map into JSONObject or parsing a {@link java.util.Collection} instance into
-     * JSONArray. The default max nesting depth is 512, which means the parser will throw a JsonException
-     * if the maximum depth is reached.
+     * Defines the maximum nesting depth that the parser will descend before throwing an exception when parsing a map
+     * into JSONObject or parsing a {@link java.util.Collection} instance into JSONArray. The default max nesting depth
+     * is 512, which means the parser will throw a JsonException if the maximum depth is reached.
      *
      * @param maxNestingDepth the maximum nesting depth allowed to the JSON parser
      * @return The existing configuration will not be modified. A new configuration is returned.
@@ -44,9 +51,8 @@ public class JSONParserConfiguration extends ParserConfiguration {
     }
 
     /**
-     * Controls the parser's behavior when meeting duplicate keys.
-     * If set to false, the parser will throw a JSONException when meeting a duplicate key.
-     * Or the duplicate key's value will be overwritten.
+     * Controls the parser's behavior when meeting duplicate keys. If set to false, the parser will throw a
+     * JSONException when meeting a duplicate key. Or the duplicate key's value will be overwritten.
      *
      * @param overwriteDuplicateKey defines should the parser overwrite duplicate keys.
      * @return The existing configuration will not be modified. A new configuration is returned.
@@ -58,13 +64,45 @@ public class JSONParserConfiguration extends ParserConfiguration {
         return clone;
     }
 
+
     /**
-     * The parser's behavior when meeting duplicate keys, controls whether the parser should
-     * overwrite duplicate keys or not.
+     * Sets the strict mode configuration for the JSON parser.
+     * <p>
+     * When strict mode is enabled, the parser will throw a JSONException if it encounters an invalid character
+     * immediately following the final ']' character in the input. This is useful for ensuring strict adherence to the
+     * JSON syntax, as any characters after the final closing bracket of a JSON array are considered invalid.
+     *
+     * @param mode a boolean value indicating whether strict mode should be enabled or not
+     * @return a new JSONParserConfiguration instance with the updated strict mode setting
+     */
+    public JSONParserConfiguration withStrictMode(final boolean mode) {
+        JSONParserConfiguration clone = this.clone();
+        clone.strictMode = mode;
+
+        return clone;
+    }
+
+    /**
+     * The parser's behavior when meeting duplicate keys, controls whether the parser should overwrite duplicate keys or
+     * not.
      *
      * @return The <code>overwriteDuplicateKey</code> configuration value.
      */
     public boolean isOverwriteDuplicateKey() {
         return this.overwriteDuplicateKey;
+    }
+
+
+    /**
+     * Retrieves the current strict mode setting of the JSON parser.
+     * <p>
+     * Strict mode, when enabled, instructs the parser to throw a JSONException if it encounters an invalid character
+     * immediately following the final ']' character in the input. This ensures strict adherence to the JSON syntax, as
+     * any characters after the final closing bracket of a JSON array are considered invalid.
+     *
+     * @return the current strict mode setting. True if strict mode is enabled, false otherwise.
+     */
+    public boolean isStrictMode() {
+        return this.strictMode;
     }
 }
