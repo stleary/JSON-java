565bfa2a362061851ca7ff5d66e3037db2993994
TANUJ VIPULKUMAR DOSHI <tn716378@dal.ca>
2024-03-30 17:04:38 -0300

    Removed: unnecessary line

--- src/main/java/org/json/TokenHandler.java
+++ src/main/java/org/json/TokenHandler.java
@@ -2,6 +2,5 @@ package org.json;
 
 
 abstract class TokenHandler {
-    // abstract boolean handleToken(XMLTokener x, JSONObject context, String name, XMLParserConfiguration config, int currentNestingDepth) throws JSONException;
     abstract boolean handleToken(XMLTokener x, JSONObject context, XMLParserConfiguration config, int currentNestingDepth, Object token, String name) throws JSONException;
 }
