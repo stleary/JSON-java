ac65ee0490d92f6b1854d22651a8e8ded8b7c5ec
md-yasir <mohdyasir231990@gmail.com>
2025-10-25 20:32:30 +0530

    Revert "Refactored stop conditions to be invariant by using while loop."
    This issue can be ignored

--- src/main/java/org/json/Cookie.java
+++ src/main/java/org/json/Cookie.java
@@ -191,30 +191,21 @@ public class Cookie {
      * @return The unescaped string.
      */
     public static String unescape(String string) {
-        int i = 0;
         int length = string.length();
         StringBuilder sb = new StringBuilder(length);
-
-        while (i < length) {
+        for (int i = 0; i < length; ++i) {
             char c = string.charAt(i);
             if (c == '+') {
-                sb.append(' ');
-                i++;
+                c = ' ';
             } else if (c == '%' && i + 2 < length) {
                 int d = JSONTokener.dehexchar(string.charAt(i + 1));
                 int e = JSONTokener.dehexchar(string.charAt(i + 2));
-
                 if (d >= 0 && e >= 0) {
-                    sb.append((char)(d * 16 + e));
-                    i += 3;
-                } else {
-                    sb.append(c);
-                    i++;
+                    c = (char)(d * 16 + e);
+                    i += 2;
                 }
-            } else {
-                sb.append(c);
-                i++;
             }
+            sb.append(c);
         }
         return sb.toString();
     }
